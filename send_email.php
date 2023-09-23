b5db45510fa070342d1084132ea11755f9d54734
VIGNESHWARAN N <125790147+vicky17N@users.noreply.github.com>
2023-09-23 12:59:44 +0530

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -16,12 +16,12 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->Host       = 'smtp-relay.brevo.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'vicky6369ece@gmail.com';                     //SMTP username
-    $mail->Password   = 'slnz puxc bses dzqb';                               //SMTP password
+    $mail->Password   = 'KFwLHcTV9GJbRAIZ';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
     $mail->setFrom('from@example.com', 'Mailer');
